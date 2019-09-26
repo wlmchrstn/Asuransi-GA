95ad394dce01cf0dd21c29fc129eff1d0bda90b6
William Christian <wlmchrstn@gmai.com>
2019-09-26 14:08:51 +0700

    [will] add auth on comment

--- routes/v1/comment.js
+++ routes/v1/comment.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const commentRouter = require('../../controllers/commentController');
+const auth = require('../../middlewares/auth');
 
-router.post('/:insurance', commentRouter.addComment)
-router.put('/:comment', commentRouter.editComment)
-router.delete('/:comment', commentRouter.deleteComment)
-router.get('/:insurance', commentRouter.getAllComment)
+router.post('/:insurance', auth.isAuthenticated, commentRouter.addComment)
+router.put('/:comment', auth.isAuthenticated, commentRouter.editComment)
+router.delete('/:comment', auth.isAuthenticated, commentRouter.deleteComment)
+router.get('/:insurance', auth.isAuthenticated, commentRouter.getAllComment)
 
 module.exports = router;
