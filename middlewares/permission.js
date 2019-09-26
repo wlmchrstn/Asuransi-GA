6ab833bbf5818a1305c5daf1dd34ffaeb6d45586
ayumhrn <maharani.ayu98@gmail.com>
2019-09-26 13:37:54 +0700

    [ayu]modify permission

--- middlewares/permission.js
+++ middlewares/permission.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if (req.decoded.role !== 'Admin') {
-        return res.status(401).json({
+        return res.status(403).json({
             message: 'You are not authorized'
         })
     }
