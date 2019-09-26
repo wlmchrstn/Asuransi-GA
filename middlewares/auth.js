fec9b1a17f16aa4e06d70f63d625154f4fcb9981
ayumhrn <maharani.ayu98@gmail.com>
2019-09-26 11:46:55 +0700

    [ayu]create permission

---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+var jwt = require('jsonwebtoken');
+
+exports.isAuthenticated = function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers.authorization
+    if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+            if (err) {
+                res.json({message: 'Failed to authenticate token'})
+            }
+            else {
+                req.decoded = decoded;
+                next()
+            }
+        });
+    }
+    else {
+        return res.status(403).send({
+            message: 'No token provided'
+        })
+    }
+}
--- routes/v1/insurance.js
+++ routes/v1/insurance.js
@@ -2,18 +2,20 @@ var express = require('express');
 var router = express.Router();
 var insuranceController = require('../../controllers/insuranceController');
 var upload = require('../../middlewares/multer');
+var auth = require('../../middlewares/auth');
+var permission = require('../../middlewares/permission')
 
 
-router.post('/', insuranceController.createInsurance);
+router.post('/', auth.isAuthenticated, permission, insuranceController.createInsurance);
 
-router.post('/:id', upload.single('image'), insuranceController.uploadphoto);
+router.post('/:id', auth.isAuthenticated, permission, upload.single('image'), insuranceController.uploadphoto);
 
 router.get('/', insuranceController.ShowAllInsurance);
 
 router.get('/:id', insuranceController.ShowOneInsurance);
 
-router.put('/:id', insuranceController.updateInsurance);
+router.put('/:id', auth.isAuthenticated, permission, insuranceController.updateInsurance);
 
-router.delete('/delete/:id', insuranceController.deleteInsurance);
+router.delete('/delete/:id', auth.isAuthenticated, permission, insuranceController.deleteInsurance);
 
 module.exports = router;
