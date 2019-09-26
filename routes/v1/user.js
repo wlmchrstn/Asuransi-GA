58c4e2560eb6064d20aee1c8585628fc436006f1
Aprima Agam <agam.w11@gmail.com>
2019-09-26 15:03:29 +0700

    [agam] modify userRouter

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,10 +1,9 @@
 const router = require('express').Router()
-const auth = require('../../middlewares/auth')
+const {isAuthenticated}= require('../../middlewares/auth')
 const permission = require('../../middlewares/permission')
 const {createSuperAdmin, createAdmin, createClient, login, 
         verify, resendVerify, show, update, deleteUser,
         uploadImage, showAdmin} = require('../../controllers/userController')//initiate userController
-//const {authenticate, merchantAuthenticate, customerAuthenticate} = require('../middlewares/authenticate')
 
 router.post('/super', createSuperAdmin)// create super admin router
 router.post('/admin', createAdmin)// create admin router
@@ -12,11 +11,11 @@ router.post('/client', createClient)// create client router
 router.post('/login', login)// login router
 router.get('/verify/:token', verify)// email verify router
 router.post('/resendverify', resendVerify)// resend email verify router
-router.get('/show/:id', auth, show)// show user router
-router.put('/update/:id', auth, update)// update user router
-router.delete('/delete/:id', auth, permission, deleteUser)// delete user router
-router.post('/upload/:id', auth, uploadImage)// Upload image user router
-router.get('/showAdmin/', auth, permission, showAdmin)// show all admin user router
+router.get('/show', isAuthenticated, show)// show user router
+router.put('/update', isAuthenticated, update)// update user router
+router.delete('/delete/:id', isAuthenticated, permission, deleteUser)// delete user router
+router.post('/upload/:id', isAuthenticated, uploadImage)// Upload image user router
+router.get('/showAdmin/', isAuthenticated, permission, showAdmin)// show all admin user router
 
 
 
